640287cc560efa475c8a57eaaedf77b300dcda1f
Claire Thiebaut <claire.thiebaut.1@gmail.com>
2021-05-31 15:58:54 +0200

    PRODUCT fct displayOneCamera - INDEX with Boostrap

---
+++ frontend/views/cart.js
@@ -0,0 +1,18 @@
+cart.innerHTML += `<form method="post"> 
+
+                    <label for="firstName">Prénom</label>
+                    <input type="text" name="firstName"></input>
+                    
+                    <label for="lastName">Nom</label>
+                    <input type="text" name="firstName"></input>
+
+                    <label for="address">Adresse</label>
+                    <input type="text" name="adresse"></input>
+
+                    <label for="city">Ville</label>
+                    <input type="text" name="city"></input>
+
+                    <label for="email">adresse email</label>
+                    <input type="email" name="email"></input>
+                </form>`
+        
